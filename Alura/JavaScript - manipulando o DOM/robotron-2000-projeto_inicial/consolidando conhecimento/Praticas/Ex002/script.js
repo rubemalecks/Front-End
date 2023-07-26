bf19a4eadecfc365de8be0fc3b1f4c5ae91262c2
Rubem Álecks <rubemalecks@gmail.com>
2023-07-26 14:55:28 -0300

    ...

---
+++ Alura/JavaScript - manipulando o DOM/robotron-2000-projeto_inicial/consolidando conhecimento/Praticas/Ex002/script.js
@@ -0,0 +1,9 @@
+var varMonitor = document.querySelector("#input-text");
+var conteudoInput = document.querySelector("#output-paragraph").value;
+
+function monitor (){
+    conteudoInput = varMonitor;
+
+}
+
+
