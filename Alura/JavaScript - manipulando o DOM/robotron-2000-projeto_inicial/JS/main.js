84c0aa5d3cdf2ec9b4d08d73a9517a7a7af79950
Rubem Álecks <rubemalecks@gmail.com>
2023-08-01 16:03:28 -0300

    ...

--- Alura/JavaScript - manipulando o DOM/robotron-2000-projeto_inicial/JS/main.js
+++ Alura/JavaScript - manipulando o DOM/robotron-2000-projeto_inicial/JS/main.js
@@ -1,10 +1,12 @@
 const robotron = document.querySelector("#robotron")
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
 
-robotron.addEventListener("click", (evento) => {
-    console.log(evento)
+somar.addEventListener("click", () => {
+    braco.value = parseInt(braco.value) + 1
 })
 
-function dizOi() { 
-    console.log("oi")
-}
-
+subtrair.addEventListener("click", () => {
+    braco.value = parseInt(braco.value) - 1
+})
