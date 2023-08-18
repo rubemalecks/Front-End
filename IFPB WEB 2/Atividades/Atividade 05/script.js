05274438d6542147006d1d4b7469f7bbe43285b1
Rubem Álecks <rubemalecks@gmail.com>
2023-08-18 15:47:14 -0300

    ...

--- IFPB WEB 2/Atividades/Atividade 05/script.js
+++ IFPB WEB 2/Atividades/Atividade 05/script.js
@@ -1,52 +1,30 @@
-const chatContainer = document.querySelector('.chat-container');
-const userInput = document.getElementById('user-input');
-const sendButton = document.getElementById('send-button');
-sendButton.addEventListener('click', sendMessage);
+const quest = document.getElementById("input-container");
+const botaoEnviar = document.getElementById("send-button");
+const OPENAI_API_KEY = ""
 
-function sendMessage() {
-    const message = userInput.value;
-    if (message.trim() !== '') {
-        const userMessageElement = createMessageElement(`Você: ${message}`);
-        chatContainer.appendChild(userMessageElement);
-        userInput.value = '';
 
-        // Substitua 'SUA_CHAVE_DE_API_DA_OPENAI_AQUI' pela sua chave de API real
-        const apiKey = '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';
-        const apiUrl = 'https://api.openai.com/v1/chat/completions';
-        
-        fetch(apiUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${apiKey}`,
-            },
-            body: JSON.stringify({
-                model: 'gpt-3.5-turbo',
-                messages: [
-                    {
-                        role: 'user',
-                        content: message,
-                    },
-                ],
-            }),
+botaoEnviar.addEventListener("click", async(e) => {
+    let pergunta = document.getElementById("user-input").value;
+    console.log(pergunta);
+
+    // Requisição para chatgpt
+
+    await fetch("https://api.openai.com/v1/completions", {
+        // Método de Envio
+        method: "POST", 
+
+        // Dados de Cabeçalho 
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + OPENAI_API_KEY,
+        },
+
+        body: JSON.stringify({
+            model: "text-davinci-003", //Modelo
+            prompt: pergunta, 
+            max_tokens: 2048,
+            temperature: 0.5
         })
-        .then(response => response.json())
-        .then(data => {
-            if (data && data.choices && data.choices.length > 0) {
-                const gptResponse = data.choices[0].message.content;
-                const gptMessageElement = createMessageElement(`ChatGPT: ${gptResponse}`);
-                chatContainer.appendChild(gptMessageElement);
-            } else {
-                console.error('Resposta inválida da API do ChatGPT:', data);
-            }
-        })
-        .catch(error => console.error('Erro ao enviar mensagem para o ChatGPT:', error));
-    }
-}
-
-function createMessageElement(text) {
-    const messageElement = document.createElement('div');
-    messageElement.classList.add('message');
-    messageElement.textContent = text;
-    return messageElement;
-}
+    });
+})
